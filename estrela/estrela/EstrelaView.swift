f32e98bb09f44f32baa37d28724cbb2f75d7beef
HC2MAC13 <>
2018-03-07 19:52:06 -0300

    estrela e joaninha

--- estrela/estrela/EstrelaView.swift
+++ estrela/estrela/EstrelaView.swift
@@ -1,9 +1,8 @@
 //
-//  TriangleView.swift
-//  primeiroDesenho
+//  EstrelaView.swift
+//  Estrela dinâmica
 //
-//  Created by HC2MAC13 on 28/02/18.
-//  Copyright © 2018 HC2MAC13. All rights reserved.
+//  Created by Bruno Guaitanele on 28/02/18.
 //
 import UIKit
 
@@ -28,77 +27,39 @@ class EstrelaView : UIView{
     @IBInspectable
     var raioGrande: CGFloat = 100
     
+    /**
+     * O desenho é feito utilizando duas circunferências, uma grande e uma pequena
+     * A estrela é feita alternando a cada desenho entre a circunferência. Caso, as
+     * duas circunferências tenham o mesmo tamanho, o desenho final é uma forma com o
+     * dobro de pontos do número de braços (ex: uma estrela de 5 pontos vira um decagono)
+     *
+     * É feito inicialmente a divisão de ângulos de acordo com o número de pontos. Para a divisão
+     * foi utilizado o número 6.283185 que é o radiano de 360.
+     * É feito então um loop passando por cada um desses ângulos, utilizando as seguintes formas para
+     * extrair as coordenadas x,y do ponto a ser desenhado:
+     *
+     * x: cosseno(angulo) x raio
+     * y: seno(angulo) x raio
+     *
+     * Lembrando que o raio em cada etapa do loop se alternará entre o grande e o pequeno.
+     * Somam-se aos pontos x e y o valor do ponto do centro, para que a estrela fique centrelizada.
+     */
     override func draw( _ rect: CGRect){
         
-        let tamanhoAngulo = 360/pontos
+        pontos *= 2
+        let path = UIBezierPath()
         var raio = raioGrande
         let centro = CGPoint(x: rect.width/2, y: rect.height/2)
-        pontos *= 2
-        var arrPontos: [CGPoint] = []
-        var ponto : CGFloat = 0;
-        var coordenada = CGPoint(x: centro.x , y: centro.y - raio )
-        var x:CGFloat!, y:CGFloat!,anguloRad : CGFloat!,angulo : CGFloat!
-        
-        let path = UIBezierPath()
-        
-        path.move(to: coordenada )
-        
+        let tamanhoAngulo = 6.283185/pontos
+        var coordenada = CGPoint(x:centro.x+raio, y:centro.y ) //ponto inicial localizado na extremidade dir. do eixo x
+        var angulo: CGFloat!
+        path.move(to: coordenada)
         raio = raioPequeno
-        while ponto <= pontos {
-            if raio == raioGrande{
-                raio = raioPequeno
-            }else{
-                raio = raioGrande
-            }
-            
+        for  ponto in stride(from: 0, to: pontos, by: 1){
+            raio = raio == raioGrande ? raioPequeno : raioGrande
             angulo = tamanhoAngulo * ponto
-            if(angulo == 0){
-                coordenada = CGPoint(x: centro.x,y: centro.y - raio )
-            }else if(angulo < 90){
-                
-                anguloRad = rad(90-angulo)
-                x = raio * cos(anguloRad )
-                y = raio * sin(anguloRad )
-                coordenada = CGPoint(x: x + centro.x , y: centro.y - y )
-                
-            }else if(angulo == 90){
-                
-                coordenada = CGPoint(x: centro.x + raio , y: centro.y)
-                
-            }else if(angulo < 180){
-                
-                anguloRad = rad(180-angulo)
-                x = raio * sin(anguloRad)+centro.x
-                y = raio * cos(anguloRad)+centro.y
-                coordenada = CGPoint(x: x, y: y)
-                
-            }else if(angulo == 180){
-                
-                coordenada = CGPoint(x: centro.x, y: centro.y+raio)
-                
-            }else if(angulo < 270){
-                
-                anguloRad = rad(270-angulo);
-                x = centro.x - raio * cos(anguloRad);
-                y = centro.y + raio * sin(anguloRad);
-                coordenada = CGPoint(x: x, y: y)
-                
-            }else if(angulo == 270){
-                
-                coordenada = CGPoint(x: centro.x - raio, y: centro.y)
-                
-            }else if(angulo < 360){
-                
-                anguloRad = rad(360-angulo);
-                x = centro.x - raio * sin(anguloRad);
-                y = centro.y - raio * cos(anguloRad);
-                coordenada = CGPoint(x: x, y:y)
-                
-            }
-            arrPontos.append(coordenada)
+            coordenada = CGPoint(x: centro.x+cos(angulo)*raio, y: centro.y+sin(angulo)*raio)
             path.addLine(to: coordenada)
-
-            ponto += 1;
         }
         
         path.close()
@@ -108,10 +69,6 @@ class EstrelaView : UIView{
         path.fill()
         path.stroke()
         
-    }
-    
-    func rad(_ degree : CGFloat ) -> CGFloat{
-        return degree * .pi / 180
     }
     
 }
